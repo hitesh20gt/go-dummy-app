0fa5cefcb94978876b0d319f759cc882a7d412a6
hitesh <hitesh20gt@gmail.com>
2025-10-22 23:19:57 +0530

    earmark pojos added

---
+++ models/EarmarkResponse.go
@@ -0,0 +1,22 @@
+package models
+
+import "math/big"
+
+type EarmarkResponse struct {
+	ID               string     `bson:"_id,omitempty" json:"id"`
+	RequestId        string     `bson:"requestId" json:"requestId"`
+	EarmarkAmount    string     `bson:"earmarkAmount" json:"earmarkAmount"`
+	EarmarkCurrency  string     `bson:"earmarkCurrency" json:"earmarkCurrency"`
+	DebitAccount     *big.Float `bson:"debitAccount" json:"debitAccount"`
+	BusinessDate     string     `bson:"businessDate" json:"businessDate"`
+	AccountBranch    string     `bson:"accountBranch" json:"accountBranch"`
+	EarmarkReference string     `bson:"earmarkReference" json:"earmarkReference"`
+	SourceSystem     string     `bson:"sourceSystem" json:"sourceSystem"`
+	CountryCode      string     `bson:"countryCode" json:"countryCode"`
+	RequestType      string     `bson:"requestType" json:"requestType"`
+	PaymentType      string     `bson:"paymentType" json:"paymentType"`
+	EarmarkType      string     `bson:"earmarkType" json:"earmarkType"`
+	ResponseCode     string     `bson:"responseCode" json:"responseCode"`
+	ResponseMessage  string     `bson:"responseMessage" json:"responseMessage"`
+	ResponseStatus   string     `bson:"responseStatus" json:"responseStatus"`
+}
